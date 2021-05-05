4c2324ac26e40e9f223dcdd8dcfe41415dc752bf
patda9 <patdanai.duangwaropas@gamil.com>
2021-05-06 04:26:31 +0700

    implement tail recursive fibonacci

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 def factorial(n: int, acc: int = 1) -> int:
   return acc if n == 0 or n == 1 else factorial(n - 1, acc * n)
 
-def fibonacci(n: int) -> int:
-  raise NotImplementedError
+def fibonacci(n: int, fn1: int = 0, fn2: int = 1) -> int:
+  return fn1 if n == 0 else (fn2 if n == 1 else fibonacci(n - 1, fn2 , fn2 + fn1))
